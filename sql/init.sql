68de900e0b6811a240b2a732488fd3d655af0b2c
Roman-Statsura <srs1012@yandex.ru>
2023-06-12 21:14:23 +0400

    initial files

---
+++ sql/init.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE effort;
+
+\c board;
+
+CREATE TABLE users(
+  email text NOT NULL,
+  hashedPassword text NOT NULL,
+  firstName text,
+  lastName text,
+  company text,
+  role text NOT NULL
+);
+
+ALTER TABLE
+  users
+ADD
+  CONSTRAINT pk_users PRIMARY KEY (email);
+
+CREATE TABLE recoverytokens (
+  email text NOT NULL,
+  token text NOT NULL,
+  expiration bigint NOT NULL
+);
+
+ALTER TABLE
+  recoverytokens
+ADD
+  CONSTRAINT pk_recoverytokens PRIMARY KEY (email);
---
+++ src/main/scala/com/srs/effort/domain/user.scala
@@ -0,0 +1,41 @@
+package com.srs.effort.domain
+
+import doobie.util.meta.Meta
+import tsec.authorization.SimpleAuthEnum
+import tsec.authorization.AuthGroup
+
+object user {
+  final case class User(
+      email: String,
+      hashedPassword: String,
+      firstName: Option[String],
+      lastName: Option[String],
+      company: Option[String],
+      role: Role
+  ) {
+    def isAdmin: Boolean = role == Role.ADMIN
+    def isRecruiter: Boolean = role == Role.RECRUITER
+  }
+
+  final case class NewUserInfo(
+      email: String,
+      password: String,
+      firstName: Option[String],
+      lastName: Option[String],
+      company: Option[String]
+  )
+
+  enum Role {
+    case ADMIN, RECRUITER
+  }
+
+  object Role {
+    given metaRole: Meta[Role] =
+      Meta[String].timap[Role](Role.valueOf)(_.toString)
+  }
+
+  given roleAuthEnum: SimpleAuthEnum[Role, String] with {
+    override val values: AuthGroup[Role]     = AuthGroup(Role.ADMIN, Role.RECRUITER)
+    override def getRepr(role: Role): String = role.toString
+  }
+}
